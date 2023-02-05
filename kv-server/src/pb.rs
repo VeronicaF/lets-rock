3cbacca3ad8b59f851824b22b1368faf721d08a0
veronicaf <1204409815@qq.com>
2023-02-05 15:16:20 +0800

    0x01: kv-server with sled

--- kv-server/src/pb.rs
+++ kv-server/src/pb.rs
@@ -2,7 +2,9 @@ pub mod abi;
 
 use crate::KvError;
 use abi::{command_request::RequestData, *};
+use bytes::Bytes;
 use http::StatusCode;
+use prost::Message;
 
 impl CommandRequest {
     pub fn new_hset(table: impl Into<String>, key: impl Into<String>, value: Value) -> Self {
@@ -36,6 +38,15 @@ impl Kvpair {
     pub fn new(key: impl Into<String>, value: Value) -> Self {
         Self {
             key: key.into(),
+            value: Some(value),
+        }
+    }
+}
+
+impl From<(String, Value)> for Kvpair {
+    fn from((key, value): (String, Value)) -> Self {
+        Self {
+            key,
             value: Some(value),
         }
     }
@@ -103,5 +114,83 @@ impl From<KvError> for CommandResponse {
             _ => {}
         }
         result
+    }
+}
+
+impl From<bool> for Value {
+    fn from(b: bool) -> Self {
+        Self {
+            value: Some(value::Value::Bool(b)),
+        }
+    }
+}
+
+impl From<f64> for Value {
+    fn from(f: f64) -> Self {
+        Self {
+            value: Some(value::Value::Float(f)),
+        }
+    }
+}
+
+impl TryFrom<Value> for i64 {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Integer(i)) => Ok(i),
+            _ => Err(KvError::ConvertError(v, "Integer")),
+        }
+    }
+}
+
+impl TryFrom<Value> for f64 {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Float(f)) => Ok(f),
+            _ => Err(KvError::ConvertError(v, "Float")),
+        }
+    }
+}
+
+impl TryFrom<Value> for Bytes {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Binary(b)) => Ok(b),
+            _ => Err(KvError::ConvertError(v, "Binary")),
+        }
+    }
+}
+
+impl TryFrom<Value> for bool {
+    type Error = KvError;
+
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        match v.value {
+            Some(value::Value::Bool(b)) => Ok(b),
+            _ => Err(KvError::ConvertError(v, "Boolean")),
+        }
+    }
+}
+
+impl TryFrom<Value> for Vec<u8> {
+    type Error = KvError;
+    fn try_from(v: Value) -> Result<Self, Self::Error> {
+        let mut buf = Vec::with_capacity(v.encoded_len());
+        v.encode(&mut buf)?;
+        Ok(buf)
+    }
+}
+
+impl TryFrom<&[u8]> for Value {
+    type Error = KvError;
+
+    fn try_from(data: &[u8]) -> Result<Self, Self::Error> {
+        let msg = Value::decode(data)?;
+        Ok(msg)
     }
 }
--- kv-server/src/service.rs
+++ kv-server/src/service.rs
@@ -22,32 +22,113 @@ impl<Store> Clone for Service<Store> {
         }
     }
 }
+
 /// Service 内部数据结构
 pub struct ServiceInner<Store> {
     store: Store,
+    on_received: Vec<fn(&CommandRequest) -> Result<(), KvError>>,
+    on_executed: Vec<fn(&CommandResponse) -> Result<(), KvError>>,
+    on_before_send: Vec<fn(&mut CommandResponse) -> Result<(), KvError>>,
+    on_after_send: Vec<fn() -> Result<(), KvError>>,
 }
+
+impl<Store: Storage> ServiceInner<Store> {
+    pub fn new(store: Store) -> Self {
+        Self {
+            store,
+            on_received: vec![],
+            on_executed: vec![],
+            on_before_send: vec![],
+            on_after_send: vec![],
+        }
+    }
+
+    pub fn fn_received(mut self, f: fn(&CommandRequest) -> Result<(), KvError>) -> Self {
+        self.on_received.push(f);
+        self
+    }
+    pub fn fn_executed(mut self, f: fn(&CommandResponse) -> Result<(), KvError>) -> Self {
+        self.on_executed.push(f);
+        self
+    }
+    pub fn fn_before_send(mut self, f: fn(&mut CommandResponse) -> Result<(), KvError>) -> Self {
+        self.on_before_send.push(f);
+        self
+    }
+    pub fn fn_after_send(mut self, f: fn() -> Result<(), KvError>) -> Self {
+        self.on_after_send.push(f);
+        self
+    }
+}
+
+impl<Store: Storage> From<ServiceInner<Store>> for Service<Store> {
+    fn from(inner: ServiceInner<Store>) -> Self {
+        Self {
+            inner: Arc::new(inner),
+        }
+    }
+}
+
 impl<Store: Storage> Service<Store> {
     pub fn new(store: Store) -> Self {
         Self {
-            inner: Arc::new(ServiceInner { store }),
+            inner: Arc::new(ServiceInner::new(store)),
         }
     }
     pub fn execute(&self, cmd: CommandRequest) -> CommandResponse {
         debug!("Got request: {:?}", cmd);
-        // TODO: 发送 on_received 事件
-        let res = dispatch(cmd, &self.inner.store);
+        if let Err(e) = self.inner.on_received.notify(&cmd) {
+            return e.into();
+        }
+        let mut res = dispatch(cmd, &self.inner.store);
         debug!("Executed response: {:?}", res);
-        // TODO: 发送 on_executed 事件
+        if let Err(e) = self.inner.on_executed.notify(&res) {
+            return e.into();
+        }
+        if let Err(e) = self.inner.on_before_send.notify(&mut res) {
+            return e.into();
+        }
+        if !self.inner.on_before_send.is_empty() {
+            debug!("Modified response: {:?}", res);
+        }
         res
     }
 }
+
+/// 事件通知（不可变事件）
+pub trait Notify<Arg> {
+    fn notify(&self, arg: &Arg) -> Result<(), KvError>;
+}
+/// 事件通知（可变事件）
+pub trait NotifyMut<Arg> {
+    fn notify(&self, arg: &mut Arg) -> Result<(), KvError>;
+}
+
+impl<Arg> Notify<Arg> for Vec<fn(&Arg) -> Result<(), KvError>> {
+    #[inline]
+    fn notify(&self, arg: &Arg) -> Result<(), KvError> {
+        for f in self {
+            f(arg)?
+        }
+        Ok(())
+    }
+}
+impl<Arg> NotifyMut<Arg> for Vec<fn(&mut Arg) -> Result<(), KvError>> {
+    #[inline]
+    fn notify(&self, arg: &mut Arg) -> Result<(), KvError> {
+        for f in self {
+            f(arg)?
+        }
+        Ok(())
+    }
+}
+
 // 从 Request 中得到 Response，目前处理 HGET/HGETALL/HSET
 pub fn dispatch(cmd: CommandRequest, store: &impl Storage) -> CommandResponse {
     match cmd.request_data {
         Some(RequestData::Hget(param)) => param.execute(store),
         Some(RequestData::Hgetall(param)) => param.execute(store),
         Some(RequestData::Hset(param)) => param.execute(store),
-        Some(RequestData::Hmget(param)) => param.execute(store),
         None => KvError::InvalidCommand("Request has no data".into()).into(),
         _ => KvError::Internal("Not implemented".into()).into(),
     }
@@ -57,11 +138,14 @@ pub fn dispatch(cmd: CommandRequest, store: &impl Storage) -> CommandResponse {
 mod tests {
     use super::*;
     use crate::{MemTable, Value};
+    use http::StatusCode;
     use std::thread;
+    use tracing::info;
+
     #[test]
     fn service_should_works() {
         // 我们需要一个 service 结构至少包含 Storage
-        let service = Service::new(MemTable::default());
+        let service: Service = ServiceInner::new(MemTable::default()).into();
         // service 可以运行在多线程环境下，它的 clone 应该是轻量级的
         let cloned = service.clone();
         // 创建一个线程，在 table t1 中写入 k1, v1
@@ -73,6 +157,37 @@ mod tests {
         // 在当前线程下读取 table t1 的 k1，应该返回 v1
         let res = service.execute(CommandRequest::new_hget("t1", "k1"));
         assert_res_ok(res, &["v1".into()], &[]);
+    }
+
+    #[test]
+    fn event_registration_should_work() {
+        fn b(cmd: &CommandRequest) -> Result<(), KvError> {
+            info!("Got {:?}", cmd);
+            Ok(())
+        }
+        fn c(res: &CommandResponse) -> Result<(), KvError> {
+            info!("{:?}", res);
+            Ok(())
+        }
+        fn d(res: &mut CommandResponse) -> Result<(), KvError> {
+            res.status = StatusCode::CREATED.as_u16() as _;
+            Ok(())
+        }
+        fn e() -> Result<(), KvError> {
+            info!("Data is sent");
+            Ok(())
+        }
+        let service: Service = ServiceInner::new(MemTable::default())
+            .fn_received(|_: &CommandRequest| Ok(()))
+            .fn_received(b)
+            .fn_executed(c)
+            .fn_before_send(d)
+            .fn_after_send(e)
+            .into();
+        let res = service.execute(CommandRequest::new_hset("t1", "k1", "v1".into()));
+        assert_eq!(res.status, StatusCode::CREATED.as_u16() as _);
+        assert_eq!(res.message, "");
+        assert_eq!(res.values, vec![Value::default()]);
     }
 }
 use crate::command_request::RequestData;
